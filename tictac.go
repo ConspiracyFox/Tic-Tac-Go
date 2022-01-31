498e10cc04e2a4b0a5d98871316efd9a36b15a9b
MentorDerGrosse <90867261+MentorDerGrosse@users.noreply.github.com>
2022-01-31 01:18:42 +0100

    Update tictac.go

--- tictac.go
+++ tictac.go
@@ -1,80 +1,76 @@
 package main
 
 import "fmt"
+import "strings"
 
-func main() {
-
-	//the game board -> empty Strings
-
-	ticBoard := [3][3]string{}
-
-	// var to carry current player name
-
+func printBoardPlay() {
+  ticBoard := [9]string{}
 	player := "x"
 
 	for {
+		fmt.Println(" ",ticBoard[0], " | ", ticBoard[1], "|", ticBoard[2])
+		fmt.Println(" ",ticBoard[3], " | ", ticBoard[4], "|", ticBoard[5])
+		fmt.Println(" ",ticBoard[6], " | ", ticBoard[7], "|", ticBoard[8])
+		var field int
+		fmt.Println("Pick a field")
+		fmt.Scanln(&field)
 
-		fmt.Println("player", player)
-		fmt.Println(ticBoard[0])
-		fmt.Println(ticBoard[1])
-		fmt.Println(ticBoard[2])
-
-		//read row value
-
-		var row int
-		fmt.Println("Enter a number 0, 1 or 2")
-		fmt.Scanln(&row)
-
-		if row < 0 || row > 2 {
-			fmt.Println("Invalid number, please enter again: 0, 1 or 2")
+		if field < 0 || field > 8 {
+			fmt.Println("Invalid field enter again")
 			continue
 		}
-
-		//read column value
-
-		var column int
-		fmt.Println("Enter a number 0, 1 or 2")
-		fmt.Scanln(&column)
-
-		if column < 0 || column > 2 {
-			fmt.Println("Invalid number, please enter again: 0, 1 or 2")
-			continue
-		}
-
-		//set value into game ticBoard
-		if ticBoard[row][column] == "" {
-			ticBoard[row][column] = player
+		if ticBoard[field] == "" {
+			ticBoard[field] = strings.Replace(player,"","",100)
 		} else {
-			//index already has value
-			fmt.Println("invalid entry: ", row, column, "value ", ticBoard[row][column])
+			fmt.Println("Invalid field enter again")
 			continue
-
 		}
 
-		//Display the Board (after each move)
-
-		//chech if someone won
-		for i := 0; i < 3; i++ {
-			if ticBoard[i][0] == ticBoard[i][1] && ticBoard[i][1] == ticBoard[i][2] && ticBoard[i][2] == player ||
-				ticBoard[0][i] == ticBoard[1][i] && ticBoard[1][i] == ticBoard[2][i] && ticBoard[2][i] == player {
-				fmt.Println("Game over, winner is player ", player)
+		if ticBoard[0] == player {
+			if ticBoard[1] == player && ticBoard[2] == player {
+				fmt.Println(player, " won")
+				return
+			}
+			if ticBoard[3] == player && ticBoard[6] == player {
+				fmt.Println(player, " won")
+				return
+			}
+			if ticBoard[4] == player && ticBoard[8] == player {
+				fmt.Println(player, " won")
+				return
+			}
+		} else if ticBoard[8] == player {
+			if ticBoard[2] == player && ticBoard[5] == player {
+				fmt.Println(player, " won")
+				return
+			}
+			if ticBoard[6] == player && ticBoard[7] == player {
+				fmt.Println(player, " won")
+				return
+			}
+		} else if ticBoard[4] == player {
+			if ticBoard[1] == player && ticBoard[7] == player {
+				fmt.Println(player, " won")
+				return
+			}
+			if ticBoard[3] == player && ticBoard[5] == player {
+				fmt.Println(player, " won")
+				return
+			}
+			if ticBoard[2] == player && ticBoard[6] == player {
+				fmt.Println(player, " won")
 				return
 			}
 		}
-
-		if ticBoard[0][0] == ticBoard[1][1] && ticBoard[1][1] == ticBoard[2][2] && ticBoard[2][2] == player ||
-			ticBoard[0][2] == ticBoard[1][1] && ticBoard[1][1] == ticBoard[2][0] && ticBoard[2][0] == player {
-			fmt.Println("Game over, winner is player ", player)
-		}
-
-		//Swap players
-
 		if player == "x" {
 			player = "o"
 		} else {
 			player = "x"
 		}
-
 	}
+}
+
+func main() {
+	printBoardPlay()
 
 }
